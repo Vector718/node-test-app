dc3c9c2d2fa3c7d311aa26c0d11c3e0bcf1e1d08
Saswat PB <saswat3002@gmail.com>
2023-07-16 15:10:45 +0530

    Server made

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express= require('express');
+const path= require('path');
 const app= express();
 const PORT=3000;
 
-app.use()
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(__dirname)
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    
+});
+
+app.get('/test',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','gg.html'))
+})
 
 app.listen(PORT,()=>{
     console.log(`Server live on PORT : ${PORT}`);
